54aca005f052df01ad283449fffef94525263f7a
akshathsk <akshath.sk@gmail.com>
2022-10-19 01:24:14 -0500

    Updated mp2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -42,7 +42,6 @@ export default function App(this: any) {
   const [options, setOptions] = useState({ sortBy: "title", sortValue: "ASC" });
   const [genre, setGenre] = useState<GenreProps[]>([]);
   const [loading, setLoading] = useState(true);
-
   const [movieId, setMovieId] = useState<any>();
   const [index, setIndex] = useState<any>();
   const [movieDetails, setMovieDetails] = useState<any>();
@@ -84,10 +83,7 @@ export default function App(this: any) {
           "3/genre/movie/list?api_key=fded5dbbbb84c28c911ba119033c2f13&language=en-US"
         )
         .then((res) => {
-          const test =
-            '{"genres":[{"id":"28","name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"ScienceFiction"},{"id":10770,"name":"TVMovie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]}';
-          var obj = JSON.parse(test);
-          setGenre([...obj.genres]);
+          setGenre([...res.data.genres]);
         });
     getMovieList(count);
     getGenreList();
